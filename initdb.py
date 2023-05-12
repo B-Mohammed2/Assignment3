2bc3e142e4daf5d5c2550082c0c2b74c67fce96e
B-Mohammed2 <118529003+B-Mohammed2@users.noreply.github.com>
2023-05-12 16:17:49 +0100

    adding fname,lname
    connecting adding student form with db

--- initdb.py
+++ initdb.py
@@ -1,12 +1,12 @@
 import psycopg2
 
-conn=psycopg2.connect(database="flask_db3",host="localhost",user="postgres",password="Pass12",port="5432")
+conn=psycopg2.connect(database="students_details",host="localhost",user="postgres",password="Pass12",port="5432")
 
 cur=conn.cursor()
 
-cur.execute('''CREATE TABLE IF NOT EXISTS courses(id serial PRIMARY KEY, name varchar(100),fees integer, duration integer);''')
+cur.execute('''CREATE TABLE IF NOT EXISTS courses(id serial PRIMARY KEY, first_name varchar(100),last_name varchar(100)  );''')
 
-cur.execute('''INSERT INTO courses(name,fees,duration) VALUES ('python', 6500,45),('javascript',6000,30);''')
+cur.execute('''INSERT INTO courses(first_name,last_name) VALUES ('ab','ab');''')
 
 conn.commit()
 
--- students_db.py
+++ students_db.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+import psycopg2
 app=Flask(__name__)
+#connecting to DB
+def db_conn():
+    conn=psycopg2.connect(database="students_details",host="localhost",user="postgres",password="Pass12",port="5432")
+    return conn
+#route to index.html
 @app.route("/")
 def homepage():
     return render_template("index.html") 
+#route to add student webpage
 @app.route("/add_student")
 def add_student():
     return render_template("add_student.html")
+@app.route("/create",methods=['POST'])
+def create():
+    conn=db_conn()
+    cur=conn.cursor()
+    #variables to connect form with db
+    first_name=request.form['fname']
+    last_name=request.form['lname']
+    cur.execute('''INSERT INTO courses ( first_name, last_name) VALUES (%s,%s)''',(first_name,last_name))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return redirect(url_for('add_student'))
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
