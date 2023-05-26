ccc5de87ff78f01128c2e1ff1601a544366d9ace
B-Mohammed2 <118529003+B-Mohammed2@users.noreply.github.com>
2023-05-26 10:04:02 +0100

    some minor changes

--- students_db.py
+++ students_db.py
@@ -28,7 +28,7 @@ def create():
     conn.close()
     #redirecting to a function called add_student
     return redirect(url_for('add_student'))
-
+#search for student
 @app.route("/search",methods=['POST'])
 def search():
     conn=db_conn()
@@ -39,15 +39,59 @@ def search():
     ID=request.form['id']
     # cur.execute('''select * from courses where first_name=%s''')
     cur.execute('''select * from courses where id='''+ID)
-    print('''select * from courses where id='''+ID)
+    #cur.execute('''select * from courses where id=%s''',ID)
+    #print('''select * from courses where gender=%s''',ID)
     #cur.execute(query,params)
-    rows=cur.fetchall()
-    for row in rows:
-        print(row)
+    data=cur.fetchall()
+    # for row in rows:
+    #     print(row)
     cur.close()
     conn.close()
     #render_template going to the template and finding the data and display it
-    return  render_template("search_student.html", rows=rows)
+    return  render_template("search_student.html", data=data)
+
+#search student for update
+@app.route("/search_update",methods=['POST'])
+def search_update():
+    conn=db_conn()
+    cur=conn.cursor()
+   # query="select * from courses where id=%s"
+    #params=('lesly',)
+    # first_name=request.form['fname']
+    ID=request.form['id']
+    # cur.execute('''select * from courses where first_name=%s''')
+    cur.execute('''select * from courses where id='''+ID)
+    #cur.execute('''select * from courses where id=%s''',ID)
+    #print('''select * from courses where gender=%s''',ID)
+    #cur.execute(query,params)
+    data=cur.fetchall()
+    # for row in rows:
+    #     print(row)
+    cur.close()
+    conn.close()
+    #render_template going to the template and finding the data and display it
+    return  render_template("update_student.html", data=data)
+
+#update student details
+@app.route("/update",methods=['POST'])
+def update():
+    conn=db_conn()
+    cur=conn.cursor()
+    first_name=request.form['fname']
+    # last_name=request.form['lname']
+    # gender=request.form['gender']
+    # date_of_birth=request.form['date_of_birth']
+    # phone_no=request.form['phone']
+    # email=request.form['email']
+    id=request.form['id']
+    # cur.execute('''UPDATE courses SET first_name=%s,last_name=%s,gender=%s,date_of_birth=%s,phone_no=%s,email=%s WHERE id=%s'''),(first_name,last_name,gender,date_of_birth,phone_no,email,id)
+    cur.execute('''UPDATE courses SET first_name=%s WHERE id=%s''',(first_name,id))
+    # commit the changes
+    conn.commit()
+    cur.close()
+    conn.close()
+    return redirect(url_for('update_student'))
+
 #route to home webpage
 @app.route("/index")
 def index():
@@ -60,6 +104,10 @@ def add_student():
 @app.route("/search_student")
 def search_student():
     return render_template("search_student.html")
+    #route to search student webpage
+@app.route("/update_student")
+def update_student():
+    return render_template("update_student.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
