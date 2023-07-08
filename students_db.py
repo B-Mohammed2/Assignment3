0c9f687d79c9d7747c2c575fb7092a69a6aabfb2
B-Mohammed2 <118529003+B-Mohammed2@users.noreply.github.com>
2023-07-08 20:45:06 +0100

    update

--- students_db.py
+++ students_db.py
@@ -1,14 +1,23 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 import psycopg2
-user_db=os.environ.get('DB_USER')
-password_db=os.environ.get('DB_PASS')
+
+user_db = os.environ.get('DB_USER')
+password_db = os.environ.get('DB_PASS')
+l_user = os.environ.get('L_USER')
+l_pass = os.environ.get('L_PASS')
+
 app=Flask(__name__)
 
 #connecting to DB
 def db_conn():
-    conn=psycopg2.connect(database="students_details", host="dpg-cijuec5ph6euh7if58e0-a", user=user_db, password=password_db, port="5432")
+    #conn=psycopg2.connect(database="students_details", host="dpg-cijuec5ph6euh7if58e0-a", user=user_db, password=password_db, port="5432")
+    #conn=psycopg2.connect(database="students_details",host="dpg-cijuec5ph6euh7if58e0-a",user="students_details_user",password="XvSta6aWEsLkW1T9cgKgu59yoDbB1BqR",port="5432")
+    conn=psycopg2.connect(database="students_details",host="localhost",user="l_user",password="l_pass",port="5432")
     return conn
+
+print(l_user)
+print(l_pass)
 
 #route to index.html
 @app.route("/")
